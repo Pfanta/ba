c9c634317077fd2651b4f468031ede80b13eb737
Dominik Mäckel <dominik.maeckel@gmail.com>
2019-12-13 23:03:41 +0100

    Adds exception handling to RunnerUtils code invocation

--- scheduling/src/main/org/combinators/cls/scheduling/utils/RunnerUtils.java
+++ scheduling/src/main/org/combinators/cls/scheduling/utils/RunnerUtils.java
@@ -1,7 +1,8 @@
 package org.combinators.cls.scheduling.utils;
 
 import org.combinators.cls.scheduling.model.Task;
-import org.joor.Reflect;
+import org.combinators.cls.scheduling.utils.reflection.Reflect;
+import org.combinators.cls.scheduling.utils.reflection.ReflectException;
 
 import java.util.List;
 import java.util.function.Function;
@@ -11,23 +12,14 @@ public class RunnerUtils {
 		int min = Integer.MAX_VALUE;
 		
 		for(String runnerCode : runners) {
-			runnerCode = "package org.combinators.cls.scheduling;\n" +
-					             "\n" +
-					             "import org.combinators.cls.scheduling.model.Task;\n" +
-					             "\n" +
-					             "public class Runner implements java.util.function.Function<Task, Integer>{\n" +
-					             "\tpublic Integer apply(Task task) {\n" +
-					             "\t\treturn 404;\n" +
-					             "\t}\n" +
-					             "}";
-			
-			Function<Task, Integer> function = Reflect.compile("org.combinators.cls.scheduling.Runner", runnerCode).create().get();
-			
-			min = Math.min(min, function.apply(task));
+			try {
+				Function<Task, Integer> function = Reflect.compile("org.combinators.cls.scheduling.Runner", runnerCode).create().get(); //TODO: make runners return Schedule instead of finish time
+				min = Math.min(min, function.apply(task));
+			} catch(ReflectException e) {
+				ApplicationUtils.showException("Reflection exception occured", "Exception while running results", e);
+			}
 		}
 		
 		return min == Integer.MAX_VALUE ? -1 : min;
 	}
-	
-	
 }
